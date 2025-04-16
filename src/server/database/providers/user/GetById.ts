43fad14b2af10692cdd4a8b0b3bd7691e68d6465
GustavoDias496 <gustavodias4966abcd@gmail.com>
2025-04-15 22:11:05 -0300

    feat: criação do provider de GetById para usuarios

---
+++ src/server/database/providers/user/GetById.ts
@@ -0,0 +1,21 @@
+import { IUser } from "../../models/User";
+import { prisma } from '../../prisma';
+
+export const GetById = async (id: number): Promise<IUser | Error> =>{
+    try {
+        
+        const user = await prisma.user.findFirst({
+            where:{
+                id
+            }
+        });
+
+        if(user) return user;
+
+        return new Error('Usuário não encontrado')
+
+    } catch (error) {
+        console.error(error);
+        return new Error('Erro ao buscar usuário!')
+    }
+};
