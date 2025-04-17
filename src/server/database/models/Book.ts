8914b8a4ee9b3f527cb76bc65c263dbb7b46424f
GustavoDias496 <gustavodias4966abcd@gmail.com>
2025-04-16 21:53:01 -0300

    feat: criação da interface de livros

---
+++ src/server/database/models/Book.ts
@@ -0,0 +1,11 @@
+export interface IBook{
+    id: number;
+    name: string;
+    author: string;
+    publisher: string;
+    yearOfPublication: number;
+    photo: string;
+    location: string;
+    description: string;
+    categoryId: number;
+}
