a9d74414450176717d107896ffbd5f409ec7c1f8
GustavoDias496 <gustavodias4966abcd@gmail.com>
2025-04-29 20:52:04 -0300

    fix: adicionando validação no controller de criar uma categoria

--- src/server/controllers/category.ts/Create.ts
+++ src/server/controllers/category.ts/Create.ts
@@ -1,6 +1,17 @@
 import { Request, Response } from "express";
 import { CategoryProvider } from "../../database/providers/category";
 import { StatusCodes } from "http-status-codes";
+import { ICategory } from "../../database/models/Category";
+import { validation } from "../../shared/middleware";
+import * as yup from 'yup';
+
+interface IBodyProps extends Omit<ICategory, 'id'> {}
+
+export const createValidation = validation((getSchema) => ({
+    body: getSchema<IBodyProps>(yup.object().shape({
+        name: yup.string().required()
+    }))
+}));
 
 export const Create = async (req:Request, res:Response) => {
     const result = await CategoryProvider.Create(req.body);
