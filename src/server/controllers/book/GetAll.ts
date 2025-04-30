c4be90a9d102f9a0f41f4df83974843b24ae94bf
GustavoDias496 <gustavodias4966abcd@gmail.com>
2025-04-29 23:47:31 -0300

    feat: controller para obter todos os livros

---
+++ src/server/controllers/book/GetAll.ts
@@ -0,0 +1,33 @@
+import { Request, Response } from "express";
+import { validation } from "../../shared/middleware";
+import * as yup from 'yup';
+import { StatusCodes } from "http-status-codes";
+import { BookProvider } from "../../database/providers/book";
+
+interface IQueryProps {
+    page?: number;
+    limit?: number;
+}
+
+export const GetAllValidation = validation((getSchema) => ({
+  params: getSchema<IQueryProps>(yup.object().shape({
+    page: yup.number().optional().moreThan(0),
+    limit: yup.number().optional().moreThan(0),
+  })),
+}));
+
+export const GetAll = async (req:Request<{}, {}, {}, IQueryProps>, res:Response) => {
+
+    const result = await BookProvider.GetAll(req.query.limit || 10, req.query.page || 1);
+
+    if(result instanceof Error){
+        res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
+            errors: {
+                default: result.message
+            }
+        });
+        return;
+    }
+
+    res.status(StatusCodes.OK).json(result);
+};
