2a1641d6f41365c274dd259989c2663675dba03e
GustavoDias496 <gustavodias4966abcd@gmail.com>
2025-05-06 21:32:58 -0300

    feat: rota para login

--- src/server/routes/index.ts
+++ src/server/routes/index.ts
@@ -36,5 +36,6 @@ router.get('/user', userController.GetAllValidation, userController.GetAll);
 router.get('/user/:id', userController.GetByIdValidation, userController.GetById);
 router.patch('/user/:id', userController.UpdateByIdValidation, userController.UpdateById);
 router.delete('/user/:id', userController.deleteByIdValidation, userController.DeleteById);
+router.post('/user/login', userController.signInValidation, userController.SignIn);
 
 export { router };
